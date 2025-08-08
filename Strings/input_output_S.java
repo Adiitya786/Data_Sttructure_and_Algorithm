638f796c1b66c17ba975acb6c32227645af625ba
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-08 12:37:22 +0530

    Program for basic input, output and methods  in string.

---
+++ Strings/input_output_S.java
@@ -0,0 +1,23 @@
+
+public class input_output_S {
+  
+   
+
+   public static void print_string(String str) {
+      for(int i = 0; i < str.length();i++) {
+         System.out.print(str.charAt(i)+ " ");   //   pinting string
+      }
+
+      System.out.println();
+   }
+
+   public static void main(String[] var0) {
+      String firstName = "Aditya ";
+      String lastName = "Prajapati ";
+      String fullName = firstName + " " + lastName;    // concatenation
+      System.out.println(fullName);
+      System.out.println(fullName.charAt(0));   // printing a particular index of string by charAt() method.
+      print_string(fullName);
+   }
+
+}
