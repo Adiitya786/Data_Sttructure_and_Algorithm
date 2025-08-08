8e3881c126bfd75d8556f28d5ce3fcd3506e5e03
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-08 13:02:24 +0530

    Program to check whether a string is palindrome or not.

--- Strings/Palindrome_string.java
+++ Strings/Palindrome_string.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 public class Palindrome_string {
     public static boolean Palindrome(String str){
         
-        // int first =0,last =str.length();
-        // while(first<=last){
-        //     int te
-        // }
+      
 
         for(int i=0;i<str.length()/2;i++){    // run loop half times and compare both side.
             
