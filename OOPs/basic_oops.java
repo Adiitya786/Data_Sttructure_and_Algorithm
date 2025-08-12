a31b4753b99e09b1073381c2c739f63f3a11d46a
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-13 00:20:33 +0530

    Program using getter and setter in oops.

--- OOPs/basic_oops.java
+++ OOPs/basic_oops.java
@@ -1,23 +1,31 @@
 public class basic_oops {
      public static void main(String[] args) {
         student s1= new student();    
-        s1.name ="Aditya Prajapati";
-        s1.age = 19;
+        s1.set_name("Aditya Prajapati");
+        s1.set_age(19);
         s1.percentage = 87;
-        System.out.println("name of student: "+s1.name+"\nage: "+s1.age+"\npercentage: "+s1.percentage);
+        System.out.println("name of student: "+s1.get_name()+"\nage: "+s1.get_age()+"\npercentage: "+s1.percentage);
      }
 }
 
 class student{
-      String name;
-      int age;
-      int percentage;;
+      private String name;
+      private int age;
+      int percentage;
+
+    
       
-      void st_name(String new_name){
-        name = new_name;
+      void set_name(String name){
+        this.name = name;
       }
-      void st_age(int new_age){
-        age = new_age;
+      String get_name(){
+        return this.name;
+      }
+      void set_age(int age){
+        this.age = age;
+      }
+      int get_age(){
+        return this.age;
       }
       void st_perc(int chem,int maths, int phy){
          percentage = (chem+maths+phy)/3;
