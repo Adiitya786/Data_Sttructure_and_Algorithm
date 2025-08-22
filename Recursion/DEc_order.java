2d2487703ebe2065709d1d902d0a11970205b11f
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-22 19:59:44 +0530

    Proram to print numbers in increasing and decreasing order in a range   using recursion.

--- Recursion/DEc_order.java
+++ Recursion/DEc_order.java
@@ -8,7 +8,18 @@ public class DEc_order{
         System.out.print(n+" ");
         print_dec(n-1);
     }
+
+    public static void print_inc(int n){
+        if(n==0){
+            return;
+        }
+       print_inc(n-1);
+       System.out.print(n+" "); 
+        
+    }
     public static void main(String[] args) {
         print_dec(10);
+        System.out.println();
+        print_inc(10);
     }
 }
