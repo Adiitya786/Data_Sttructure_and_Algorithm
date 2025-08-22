fee49117f48bf1b45433ed10736e923d2faf385c
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-22 19:13:32 +0530

    Program to print number in decreasing order from 1 to n using recursion.

---
+++ Recursion/DEc_order.java
@@ -0,0 +1,14 @@
+public class DEc_order{
+
+    public static void print_dec(int n){
+        if(n==1){
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n+" ");
+        print_dec(n-1);
+    }
+    public static void main(String[] args) {
+        print_dec(10);
+    }
+}
