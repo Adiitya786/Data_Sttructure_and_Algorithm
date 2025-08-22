66ef6dfe4668523226de580b18c012e273f025af
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-22 19:27:55 +0530

    Program to print factorial of n numbers using recursion.

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,18 @@
+public class Fibonacci {
+
+    public static int print_fibo(int n){
+        
+        if(n==1 || n==0){  // Base case
+            return 1;
+        }
+       
+       int fib=  print_fibo(n-1);
+        int fiboo=  n * print_fibo(n-1);   //  Recursive case
+       
+       return fiboo;
+    }
+    public static void main(String[] args) {
+       
+        System.out.println( print_fibo(6));
+    }
+}
