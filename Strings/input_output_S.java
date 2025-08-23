436c34fc0ff3e41123099752e2c8ebbdef301f52
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-23 18:45:24 +0530

    Programs

--- Strings/input_output_S.java
+++ Strings/input_output_S.java
@@ -1,23 +1,18 @@
+public class input_output_S{
 
-public class input_output_S {
-  
-   
-
-   public static void print_string(String str) {
-      for(int i = 0; i < str.length();i++) {
-         System.out.print(str.charAt(i)+ " ");   //   pinting string
-      }
-
-      System.out.println();
-   }
-
-   public static void main(String[] var0) {
-      String firstName = "Aditya ";
-      String lastName = "Prajapati ";
-      String fullName = firstName + " " + lastName;    // concatenation
-      System.out.println(fullName);
-      System.out.println(fullName.charAt(0));   // printing a particular index of string by charAt() method.
-      print_string(fullName);
-   }
-
+    public static void print_string(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i)+ " ");   // Printing string 
+        }System.out.println();
+    }
+    public static void main(String[] args) {
+        
+        String firstName = "Aditya ";
+        String lastName = "Prajapati ";
+        String fullName = firstName+" "+ lastName;   // concatenation 
+        System.out.println(fullName);
+        System.out.println(fullName.charAt(0));  // using of charAt() method to print a particular index.
+        print_string(fullName);
+       
+    }
 }
