2b0cd00345d8ca1ee3efd521495b685a3ae803de
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-24 19:47:16 +0530

    Given a "2*n" board and tiles of size "2*1", count the number of ways to tile  the given board using "2*1" tiles.

---
+++ Recursion/tile_problem.java
@@ -0,0 +1,20 @@
+public class tile_problem {
+
+    public static int tile(int n){
+        // base case
+        if(n==0 ||n==1){
+            return 1;
+        }
+        // vertical tile
+        int fnm1 = tile(n-1);
+        // horizontal tile
+        int fnm2 = tile(n-2);
+        int totalWays = fnm1 + fnm2;
+        return totalWays;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tile(4));
+    }
+    
+}
