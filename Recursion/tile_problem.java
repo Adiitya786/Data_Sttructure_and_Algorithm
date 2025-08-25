7948b4ac2abea6e623282f83829a6b98a32d7c9b
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-25 19:49:20 +0530

    Optimized code of tille problem.

--- Recursion/tile_problem.java
+++ Recursion/tile_problem.java
@@ -5,16 +5,17 @@ public class tile_problem {
         if(n==0 ||n==1){
             return 1;
         }
-        // vertical tile
-        int fnm1 = tile(n-1);
-        // horizontal tile
-        int fnm2 = tile(n-2);
-        int totalWays = fnm1 + fnm2;
-        return totalWays;
+        // // vertical tile
+        // int fnm1 = tile(n-1);
+        // // horizontal tile
+        // int fnm2 = tile(n-2);
+        // int totalWays = fnm1 + fnm2;
+        // return totalWays;
+        return tile(n-1) + tile(n-2);
     }
 
     public static void main(String[] args) {
-        System.out.println(tile(5));
+        System.out.println(tile(4));
     }
     
 }
