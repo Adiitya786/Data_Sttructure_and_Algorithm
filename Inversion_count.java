87697f812543f679d62e2fb173046170b780c3ac
Adiitya786 <adityaprajapatii786@gmail.com>
2025-08-31 11:47:54 +0530

    Program that find the inversion count in an array and return the number of inversion count.

---
+++ Inversion_count.java
@@ -0,0 +1,64 @@
+public class Inversion_count {
+   
+
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+
+    public static int merge(int arr[],int si, int mid, int ei){
+        int i=si; // index for left merge
+        int j=mid+1;// index for right merge
+        int k=0;    // for temp indexing
+        int temp[] = new int[ei-si+1];
+        int invCount =0;
+        while (i<=mid && j<=ei) {
+
+            if(arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }
+            else{
+                temp[k] = arr[j];
+                invCount = invCount+ (mid-i+1);
+                j++;
+            }
+            k++;
+                
+            }
+            // for leftover elements in left array
+            while(i<=mid){
+                temp[k++] = arr[i++];
+            }
+            while(j<=ei){
+                temp[k++] = arr[j++];
+            }
+             for(k=0, i=si;k<temp.length;k++,i++){
+            arr[i] = temp[k];
+        }
+        return invCount;
+
+    }
+    public static int mergesort(int arr[], int si, int ei){
+        // Base case
+        if(si< ei){
+           
+        //kaam
+        int mid = si + (ei-si)/2;
+        int leftCount = mergesort(arr, si, mid); //left count
+        int rightCount =  mergesort(arr, mid+1, ei);
+        int mergeCount = merge(arr,si,mid,ei);
+        return leftCount+rightCount+mergeCount;
+    }
+    return 0;
+}
+    public static void main(String[] args) {
+        int arr[] = {1,3,5,10,2,6,8,9};
+        System.out.println(mergesort(arr, 0, arr.length-1));
+        
+
+    }
+
+}
