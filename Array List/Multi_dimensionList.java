51500b95ef501e170f4f5300e3085e9487d9455b
Adiitya786 <adityaprajapatii786@gmail.com>
2025-09-06 13:02:22 +0530

    Program to understand the multidimensional arraylist and their representation.

---
+++ Array List/Multi_dimensionList.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Multi_dimensionList {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>>mainlist = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
+        list2.add(2);
+        list2.add(4);
+        list2.add(6);
+        list2.add(8);
+        list2.add(10);
+        list3.add(3);
+        list3.add(6);
+        list3.add(9);
+        list3.add(12);
+        list3.add(15);
+        mainlist.add(list1);
+        mainlist.add(list2);
+        mainlist.add(list3);
+        for(int i =0;i<mainlist.size();i++){
+            ArrayList<Integer>curr = mainlist.get(i);
+            for(int j =0;j<curr.size();j++){
+                System.out.print(curr.get(j)+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println(mainlist);  // same like 2dimsional array....
+    }
+}
