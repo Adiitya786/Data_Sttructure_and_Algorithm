4358abbb449a51e4177743c0cf1022c6b15cee84
Adiitya786 <adityaprajapatii786@gmail.com>
2025-09-11 19:25:29 +0530

    Program that add and remove elements from first and last position  ,also add elements in the middle and give the size of the linked list.

--- Linked List/ad.java
+++ Linked List/ad.java
@@ -10,11 +10,13 @@ public class ad {
     }
         public static Node head;
         public static Node tail;
+        public static int size;  // automatically size initializes with 0.
         
 
         public void addfirst(int data){
            // creating a new node
             Node newNode = new  Node(data);
+            size++;
             if(head == null){
                     head = tail = newNode;
                     return;
@@ -26,6 +28,7 @@ public class ad {
         }
         public void addlast(int data){
             Node newNode = new  Node(data);
+            size++;
             if(head == null){
                     head = tail = newNode;
                     return;
@@ -35,19 +38,59 @@ public class ad {
         }
          
         public void addMiddle(int idx,int data){
-            Node newNode = new Node(data);
-            Node temp = head;
-            int i =0;
+            
             if(idx ==0){
                 addfirst(data);
                 return;
             }
+            Node newNode = new Node(data);
+            size++;
+            Node temp = head;
+            int i =0;
             while(i<idx-1){
                     temp = temp.next;
                     i++;
             }
              newNode.next = temp.next;
              temp.next = newNode;
+        }
+
+        public int removeFirst(){
+            if(size ==0){
+                System.out.println("ll is empty");
+                return Integer.MAX_VALUE;
+            }
+            if(size ==1){
+                int val = head.data;
+                head = tail = null;
+                size =0;
+                return val;
+            }
+            int val = head.data;
+                head = head.next;
+                size--;
+                return val;
+        }
+        public int removelast(){
+               if(size ==0){
+                System.out.println("ll is empty");
+                return Integer.MAX_VALUE;
+            }
+            if(size ==1){
+                int val = head.data;
+                head = tail = null;
+                size =0;
+                return val;
+            }
+            Node prev = head;
+            for(int i =0;i<size-2;i++){
+                prev = prev.next;
+            }
+            int val = prev.next.data;
+            prev.next = null;
+            tail = prev;
+            size--;
+            return val;
         }
         public void print(){
             Node temp = head;
@@ -62,17 +105,21 @@ public class ad {
         }
         public static void main(String[] args) {
             ad ll = new ad();
-            ll.print();
+            // ll.print();
            ll.addfirst(2);
-           ll.print();
+        //    ll.print();
            ll.addfirst(1);
-           ll.print();
+        //    ll.print();
            ll.addlast(3);
-           ll.print();
+        //    ll.print();
            ll.addlast(4);
            ll.addMiddle(3, 8);
+          
+        //    System.out.println("Size of the linked list: "+size);
+           ll.removeFirst();
+           ll.removelast();
            ll.print();
-           
+            System.out.println("Size of the linked list: "+size);
         }
     }
 
