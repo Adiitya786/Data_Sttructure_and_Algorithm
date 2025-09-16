2aa0d8480c8f1e3d2360ccfe307f9c37dccd4b45
Adiitya786 <adityaprajapatii786@gmail.com>
2025-09-16 23:11:51 +0530

    Program that add and remove elements of a doubly linked list from the first and last index of this doubly linked list.

---
+++ Linked List/DoubleLinklist.java
@@ -0,0 +1,96 @@
+public class DoubleLinklist {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+        public Node(int data){
+            this.data = data;
+            this.next =null;
+            this.prev =null;
+        }
+    }
+        public static Node head;
+        public static Node tail;
+        public static int size;
+
+        // to add a new element in the first index of double linked list.
+        public void addFirst(int data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = tail = newNode;
+                size++;
+                return;
+            }
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+            size++;
+        }
+          // to add element in the last position of a linked list
+         public void addLast(int data){
+            Node newNode = new Node(data);
+            if(head == null){
+                head = tail = newNode;
+                size++;
+                return;
+            }
+            tail.next = newNode;
+             newNode.prev = tail;
+             tail = newNode;
+             size++;
+
+         }
+          // to remove first element of a linked list
+        public void removefirst(){
+            if(head == null){
+                return;
+            }
+            if(size ==1){
+                head =tail = null;
+                return;
+            }
+            head = head.next;
+            head.prev = null;
+        }
+        // to remove last element of a linked list
+        public void removeLast(){
+            if(head == null){
+                return;
+            }
+            if(size ==1){
+                head =tail = null;
+                return;
+            }
+            tail.prev.next = null;
+            tail.prev = null;
+            tail = tail.prev;
+            
+                
+            
+        }
+         // to print the linked list
+        public  void print(){
+            Node temp = head;
+            while(temp != null){
+                System.out.print(temp.data+" <-> ");
+                temp = temp.next;
+            }
+            System.out.println("null");
+            System.out.println();
+        }
+    
+        public static void main(String[] args) {
+            DoubleLinklist dll  = new DoubleLinklist();
+            dll.addFirst(3);
+            dll.addFirst(2);
+            dll.addFirst(1);
+            dll.print();
+            // dll.removefirst();
+            // dll.print();
+            dll.addLast(5);
+            dll.print();
+            dll.removeLast();
+            dll.print();
+        
+    }
+}
