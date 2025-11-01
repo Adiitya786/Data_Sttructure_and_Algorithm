987fe9620dcc4e435afbc9b802765b0d52c5327b
Adiitya786 <adityaprajapatii786@gmail.com>
2025-11-01 19:05:06 +0530

    Program that find both the diameter and height of a binary tree with time complexity : O(n).

---
+++ Binary Tree/Diameter_tree2.java
@@ -0,0 +1,56 @@
+import org.w3c.dom.Node;
+
+public class Diameter_tree2 {
+    
+   
+    static class Node{
+        int data;
+        Node right;
+        Node left;
+
+        Node(int data){
+            this.data = data;
+            this.right = null;
+            this.left  =left;
+        }
+    }
+    
+    static class Info{
+        int diam;
+        int ht;
+
+        public Info(int diam,int ht){
+            this.diam = diam;
+            this.ht = ht;
+        }
+
+    }
+
+    public static Info diameter(Node root){
+        if(root == null){
+            return new Info(0, 0);
+        }
+
+        Info leftInfo = diameter(root.left);
+        Info rightInfo = diameter(root.right);
+
+        int diam = Math.max(Math.max(leftInfo.diam,rightInfo.diam),leftInfo.ht+rightInfo.ht+1);
+        int ht = Math.max(leftInfo.ht,rightInfo.ht)+1;
+
+        return new Info(diam, ht);
+    }
+
+
+    public static void main(String[] args) {
+         Node root = new Node(1);
+     root.left = new Node(2);
+     root.right = new Node(3);
+     root.left.left = new Node(4);
+     root.left.right = new Node(6);
+     root.right.left = new Node(5);
+     root.right.right = new Node(7);
+     root.left.left.left = new Node(9);
+     System.out.println(diameter(root).diam);
+     System.out.println(diameter(root).ht);
+    }
+}
