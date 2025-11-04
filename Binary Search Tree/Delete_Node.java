b50aba0fe1a70bdf3aa065a8eee8ef12eebf3f43
Adiitya786 <adityaprajapatii786@gmail.com>
2025-11-04 16:06:53 +0530

    Program that delete a node from the BST.

---
+++ Binary Search Tree/Delete_Node.java
@@ -0,0 +1,70 @@
+
+public class Delete_Node {
+    
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+            Node left = null;
+            Node right = null;
+        }
+    }public static void inorder(Node root){
+        if(root == null) return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+
+    public static Node insert(Node root, int val){
+        if(root ==null){
+            root = new Node(val);
+            return root;
+        }
+        if(root.data>val){
+            root.left = insert(root.left, val);
+        }
+        else{
+            root.right = insert(root.right, val);
+        }
+       return root;
+    }
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            findInorderSuccessor(root.left);
+        }
+        return root;
+    }
+
+    public static Node delete(Node root,int val){
+        if(root.data>val) root.left =  delete(root.left, val);
+        else if(root.data<val) root.right = delete(root.right, val);
+        else{
+        // case 1: No children
+        if(root.left == null && root.right == null) return null;
+        // case 2 : One children
+        if(root.left == null) return root.right;
+        else if(root.right == null) return root.left;
+
+        //case 3 : two children
+        Node IS = findInorderSuccessor(root.right);
+        root.data = IS.data;
+        root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        int values[] = {5,1,3,4,2,7};
+        Node root = null;
+        for(int i =0;i<values.length;i++){
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+        root = delete(root, 5);
+        inorder(root);
+    }
+
+}
